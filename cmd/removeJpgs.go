b285a2b9b00e31638f638f04050a84219bfb8e00
Nathan Boyd <nboyd@salesforce.com>
2021-02-01 08:59:59 -0500

    add testing stub for removeJpgs command

--- cmd/removeJpgs.go
+++ cmd/removeJpgs.go
@@ -1,24 +1,3 @@
-/*
-Copyright © 2021 Nathan Boyd <nathan@engineernathan.org>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package cmd
 
 import (
@@ -27,23 +6,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// removeJpgsCmd represents the removeJpgs command
-var removeJpgsCmd = &cobra.Command{
-	Use:   "removeJpgs",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+var in string
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("removeJpgs called")
-	},
+func NewRemoveJpgsCmd() *cobra.Command {
+
+	var cmd = &cobra.Command{
+		Use:   "removeJpgs",
+		Short: "something",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Fprintf(cmd.OutOrStdout(), in)
+			return nil
+		},
+	}
+	cmd.Flags().StringVar(&in, "in", "", "This is a very important input.")
+
+	return cmd
 }
 
 func init() {
-	rootCmd.AddCommand(removeJpgsCmd)
+	rootCmd.AddCommand(NewRemoveJpgsCmd())
 
 	// Here you will define your flags and configuration settings.
 
---
+++ cmd/removeJpgs_test.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+)
+
+func Test_RemoveJpgsCmd(t *testing.T) {
+	cmd := NewRemoveJpgsCmd()
+	b := bytes.NewBufferString("")
+	cmd.SetOut(b)
+	cmd.SetArgs([]string{"--in", "testisawesome"})
+	cmd.Execute()
+	out, err := ioutil.ReadAll(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(out) != "testisawesome" {
+		t.Fatalf("expected \"%s\" got \"%s\"", "testisawesome", string(out))
+	}
+}
